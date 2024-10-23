ecbeec9d3216b663edb2c416ac7b1ff6bd7d7243
litdev1 <steve.todman@live.co.uk>
2024-10-23 20:07:38 +0100

    Minor

--- app/src/main/java/com/example/taskorganiser/MainActivity.kt
+++ app/src/main/java/com/example/taskorganiser/MainActivity.kt
@@ -29,71 +29,6 @@ class MainActivity : AppCompatActivity() {
         update()
     }
 
-    fun setRecycler(data: ArrayList<Action>, adapter: CustomAdapter, recyclerview: RecyclerView) {
-        // on below line we are creating a method to create item touch helper
-        // method for adding swipe to delete functionality.
-        // in this we are specifying drag direction and position to right
-        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
-            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-        ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                // this method is called
-                // when the item is moved.
-                //return false
-                val from = viewHolder.adapterPosition
-                val to = target.adapterPosition
-                Collections.swap(data, from, to)
-                adapter.notifyItemMoved(from, to)
-                return true
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                // this method is called when we swipe our item.
-                // on below line we are getting the item at a particular position.
-                val actionItem: Action =
-                    data.get(viewHolder.adapterPosition)
-
-                // below line is to get the position
-                // of the item at that position.
-                val position = viewHolder.adapterPosition
-
-                if (direction == ItemTouchHelper.LEFT) {
-                    // this method is called when item is swiped.
-                    // below line is to remove item from our array list.
-                    data.removeAt(viewHolder.adapterPosition)
-
-                    // below line is to notify our item is removed from adapter.
-                    adapter.notifyItemRemoved(viewHolder.adapterPosition)
-
-                    // below line is to display our snackbar with action.
-                    Snackbar.make(recyclerview, "Deleted " + actionItem.text, 2000)
-                        .setAction(
-                            "Undo",
-                            View.OnClickListener {
-                                // adding on click listener to our action of snack bar.
-                                // below line is to add our item to array list with a position.
-                                data.add(position, actionItem)
-
-                                // below line is to notify item is
-                                // added to our adapter class.
-                                adapter.notifyItemInserted(position)
-                            }).show()
-                } else {
-                    actionItem.state = StateType.YES
-                    actionItem.image = R.drawable.ic_dashboard_black_24dp
-                    adapter.notifyItemChanged(position)
-                }
-            }
-            // at last we are adding this
-            // to our recycler view.
-        }).attachToRecyclerView(recyclerview)
-    }
-
     fun update()
     {
         val recyclerview = findViewById<RecyclerView>(R.id.home_recycler)
@@ -102,7 +37,8 @@ class MainActivity : AppCompatActivity() {
         recyclerview.layoutManager = LinearLayoutManager(this)
 
         // This will pass the ArrayList to our Adapter
-        val adapter = CustomAdapter(ApplicationClass.instance.task.children, this)
+        val actions = ApplicationClass.instance.task.children
+        val adapter = CustomAdapter(actions, this)
 
         // Setting the Adapter with the recyclerview
         recyclerview.adapter = adapter
@@ -110,6 +46,6 @@ class MainActivity : AppCompatActivity() {
         // that data has been updated.
         adapter.notifyDataSetChanged()
 
-        setRecycler(ApplicationClass.instance.task.children, adapter, recyclerview)
+        adapter.setRecycler(actions, adapter, recyclerview)
     }
 }
--- app/src/main/java/com/example/taskorganiser/actions/CustomAdapter.kt
+++ app/src/main/java/com/example/taskorganiser/actions/CustomAdapter.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taskorganiser.ApplicationClass
 import com.example.taskorganiser.MainActivity
 import com.example.taskorganiser.R
+import com.google.android.material.snackbar.Snackbar
+import java.util.Collections
 
 class CustomAdapter(val mList: ArrayList<Action>, val activity: MainActivity) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
@@ -43,6 +46,7 @@ class CustomAdapter(val mList: ArrayList<Action>, val activity: MainActivity) : 
             else -> holder.layoutView.setBackgroundColor(Color.WHITE)
         }
 
+        //Click
         holder.itemView.setOnClickListener { view ->
             val position = holder.layoutPosition
             val action = mList[position]
@@ -55,6 +59,7 @@ class CustomAdapter(val mList: ArrayList<Action>, val activity: MainActivity) : 
             }
         }
 
+        //Edit text
         holder.textView.setOnFocusChangeListener { view, hasFocus ->
             if (!hasFocus) {
                 val editText = view as TextView
@@ -73,5 +78,70 @@ class CustomAdapter(val mList: ArrayList<Action>, val activity: MainActivity) : 
         val imageView: ImageView = itemView.findViewById(R.id.imageView)
         val textView: TextView = itemView.findViewById(R.id.textView)
         val layoutView: LinearLayout = itemView.findViewById(R.id.layoutView)
+    }
+
+    fun setRecycler(data: ArrayList<Action>, adapter: CustomAdapter, recyclerview: RecyclerView) {
+        // on below line we are creating a method to create item touch helper
+        // method for adding swipe to delete functionality.
+        // in this we are specifying drag direction and position to right
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // this method is called
+                // when the item is moved.
+                //return false
+                val from = viewHolder.adapterPosition
+                val to = target.adapterPosition
+                Collections.swap(data, from, to)
+                adapter.notifyItemMoved(from, to)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // this method is called when we swipe our item.
+                // on below line we are getting the item at a particular position.
+                val actionItem: Action =
+                    data.get(viewHolder.adapterPosition)
+
+                // below line is to get the position
+                // of the item at that position.
+                val position = viewHolder.adapterPosition
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    // this method is called when item is swiped.
+                    // below line is to remove item from our array list.
+                    data.removeAt(viewHolder.adapterPosition)
+
+                    // below line is to notify our item is removed from adapter.
+                    adapter.notifyItemRemoved(viewHolder.adapterPosition)
+
+                    // below line is to display our snackbar with action.
+                    Snackbar.make(recyclerview, "Deleted " + actionItem.text, 2000)
+                        .setAction(
+                            "Undo",
+                            View.OnClickListener {
+                                // adding on click listener to our action of snack bar.
+                                // below line is to add our item to array list with a position.
+                                data.add(position, actionItem)
+
+                                // below line is to notify item is
+                                // added to our adapter class.
+                                adapter.notifyItemInserted(position)
+                            }).show()
+                } else {
+                    actionItem.state = StateType.YES
+                    actionItem.image = R.drawable.ic_dashboard_black_24dp
+                    adapter.notifyItemChanged(position)
+                }
+            }
+            // at last we are adding this
+            // to our recycler view.
+        }).attachToRecyclerView(recyclerview)
     }
 }
