ec162609877bf8878ebdc8f1c8f43855673d7bb9
litdev1 <steve.todman@live.co.uk>
2024-11-01 15:08:18 +0000

    Update in Android studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.litdev.taskorganiser"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.litdev.taskorganiser"
