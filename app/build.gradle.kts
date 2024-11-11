b3210776e9a7af38d0ee5cfe389e77910f4792a3
litdev1 <steve.todman@live.co.uk>
2024-11-11 17:02:35 +0000

    Multiline and some more accessibility - version 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.litdev.taskorganiser"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
