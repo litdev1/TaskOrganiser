aa52d3bcb1eed6f414b3789b0a7fddbd799b6a22
litdev1 <steve.todman@live.co.uk>
2026-09-14 17:34:07 +0100

    Version 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 
 android {
     namespace = "com.litdev.taskorganiser"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.litdev.taskorganiser"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 10
+        targetSdk = 37
+        versionCode = 11
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
